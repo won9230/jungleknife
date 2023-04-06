91e379008af87bfd2698dd07d3a37e431c287dd8
min49590 <min49590@gmail.com>
2023-04-06 15:22:33 +0900

    최종 발표 후 진짜 끝

--- Config.py
+++ Config.py
@@ -1,6 +1,10 @@
 PORT = 5000
-LOCALHOST = 'http://13.209.99.72:'+str(PORT)
-MONGODB = 'mongodb://test:1234@13.209.99.72'
+LOCALHOST = 'localhost'
+LOCALHOST_ADDRESS = 'http://:'+LOCALHOST+str(PORT)
+AWS_IP = '13.209.99.72'
+AWS_ADDRESS = 'http://'+AWS_IP+str(PORT)
+MONGODB = 'mongodb://test:1234@' + AWS_ADDRESS
+
 SECRET_KEY = 'kraftonjungle'
 ACCESS_TIME = 60 * 30
 REFRESH_TIME = 60 * 60 * 24 * 30
